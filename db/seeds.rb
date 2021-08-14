cd2755669b11eb0edad34c9d8dfccfc2a43d0a18
chris <cseiter@gmail.com>
2021-08-14 14:43:14 -0400

    2nd seed test populates both tables correctly

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,5 @@ MeritBadge.delete_all
 
 first_aid = MeritBadge.create(badges_name: "First Aid",badges_is_earned: "True",badges_is_eagle: "True")
 music = MeritBadge.create(badges_name: "Music",badges_is_earned: "True",badges_is_eagle: "False")
+
+aiden = Scout.create(scouts_f_name: "Aiden", scouts_l_name: "Seiter", scouts_troop: 1)
