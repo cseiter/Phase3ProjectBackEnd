14bf8e71fea2b38af2a9123747ee173ba4868b8c
chris <cseiter@gmail.com>
2021-08-14 12:46:49 -0400

    create_table migration changed

--- db/migrate/20210814161657_create_scouts.rb
+++ db/migrate/20210814161657_create_scouts.rb
@@ -1,4 +1,8 @@
 class CreateScouts < ActiveRecord::Migration[5.2]
   def change
+    create_table :scouts do |t|
+      t.string :scouts_f_name
+      t.string :scouts_l_name
+      t.integer :scouts_troop
   end
 end
