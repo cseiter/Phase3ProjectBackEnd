48d593a3eea7892d5a8e0072e8c5e97ed6098d8e
chris <cseiter@gmail.com>
2021-08-14 19:25:15 -0400

    fixed seed issue with boolean value

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,6 @@ MeritBadge.delete_all
 
 first_aid = MeritBadge.create(badges_name: "First_Aid",badges_is_earned: "True",badges_is_eagle: "True")
 music = MeritBadge.create(badges_name: "Music",badges_is_earned: "True",badges_is_eagle: "False")
+music.toggle!(:badges_is_eagle)
 
 aiden = Scout.create(scouts_f_name: "Aiden", scouts_l_name: "Seiter", scouts_troop: 1)
