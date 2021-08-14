fe67e69371b039ec3ab0226e5ed5ba7ce413ccf0
chris <cseiter@gmail.com>
2021-08-14 18:51:00 -0400

    replaced space with underscore in seed file and reset

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 Scout.delete_all
 MeritBadge.delete_all
 
-first_aid = MeritBadge.create(badges_name: "First Aid",badges_is_earned: "True",badges_is_eagle: "True")
+first_aid = MeritBadge.create(badges_name: "First_Aid",badges_is_earned: "True",badges_is_eagle: "True")
 music = MeritBadge.create(badges_name: "Music",badges_is_earned: "True",badges_is_eagle: "False")
 
 aiden = Scout.create(scouts_f_name: "Aiden", scouts_l_name: "Seiter", scouts_troop: 1)
