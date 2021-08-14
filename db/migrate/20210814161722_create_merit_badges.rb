99a9db8a4ad1c7936c2707dde6534833901c07c8
chris <cseiter@gmail.com>
2021-08-14 12:53:15 -0400

    merit_badges migration created

--- db/migrate/20210814161722_create_merit_badges.rb
+++ db/migrate/20210814161722_create_merit_badges.rb
@@ -1,4 +1,9 @@
 class CreateMeritBadges < ActiveRecord::Migration[5.2]
   def change
+    create_table :merit_badges do |t|
+      t.string :badges_name
+      t.boolean :badges_is_earned
+      t.boolean :badges_is_eagle
+      t.integer :scouts_id
   end
 end
