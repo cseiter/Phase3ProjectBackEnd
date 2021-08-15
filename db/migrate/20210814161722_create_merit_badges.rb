61f0007784f9cd2cb609e7def2844fc0c9e3f2b6
chris <cseiter@gmail.com>
2021-08-15 18:27:40 -0400

    starting backend db change to get rid of "boolean trap"

--- db/migrate/20210814161722_create_merit_badges.rb
+++ db/migrate/20210814161722_create_merit_badges.rb
@@ -2,8 +2,8 @@ class CreateMeritBadges < ActiveRecord::Migration[5.2]
   def change
     create_table :merit_badges do |t|
       t.string :badges_name
-      t.boolean :badges_is_earned
-      t.boolean :badges_is_eagle
+      t.string :badges_is_earned
+      t.string :badges_is_eagle
       t.integer :scouts_id
     end
   end
