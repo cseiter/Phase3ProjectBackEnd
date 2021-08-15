e5350152357291def967a5ab55a325f3744a7c60
chris <cseiter@gmail.com>
2021-08-15 13:04:52 -0400

    update seed file a bit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,16 @@
 Scout.delete_all
 MeritBadge.delete_all
 
+#Eagle required
+#First Aid, Citizenship in the Community, Citizenship in the Nation, Citizenship in the World,
+#Communication, Cooking, Personal Fitness, Emergency Preparedness OR Lifesaving,
+#Environmental Science OR Sustainability, Personal Management, Camping, Family Life,
+#Swimming OR Hiking OR Cycling
+
 first_aid = MeritBadge.create(badges_name: "First_Aid",badges_is_earned: "True",badges_is_eagle: "True")
 music = MeritBadge.create(badges_name: "Music",badges_is_earned: "True",badges_is_eagle: "False")
+wilderness_survival = MeritBadge.create(badges_name: "Wilderness_Survival",badges_is_earned: "True",badges_is_eagle: "False")
+
 music.toggle!(:badges_is_eagle)
 
 aiden = Scout.create(scouts_f_name: "Aiden", scouts_l_name: "Seiter", scouts_troop: 1)
